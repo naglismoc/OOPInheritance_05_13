7766d7a88f7e77d1aae61b89292b02dda0a77a42
naglismoc <dvd.filmas@gmail.com>
2024-05-27 16:19:12 +0300

    rekursijos example

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,22 @@ public class Employee extends Object{
     private String occupation;
     private double salary;
 
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,71 @@
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+            Užduotis:
+            OOP CRUD
+            Author:
+            long id
+            string name
+            string surname
+         */
+
+
         System.out.println("Hello world!");
         Student s = new Student();
         s.introduce();
         System.out.println(s);
         System.out.println(s.toString());
+
+        ArrayList<Employee> ultimateWorkForce = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+//        while (true){
+//            int input = sc.nextInt();
+//            switch (input){
+//                case 1:
+//                    for (Employee e : ultimateWorkForce) {
+//                        System.out.println(e);
+//                    }
+//                break;
+//                case 2:
+//                    Employee emp = new Employee();
+//                    System.out.println("iveskite darboviete:");
+//                    emp.setOccupation(sc.nextLine());
+//                    System.out.println("iveskite algos dydi:");
+//                    double salary = sc.nextDouble();
+//                    sc.nextLine();
+//                    emp.setSalary(salary);
+//                    ultimateWorkForce.add(emp);
+//                break;
+//            }
+//
+//        }
+        // Replace "C:\\" with the actual path you want to explore (adjust for OS)
+        String rootPath = "C:\\Users\\dvdfi\\OneDrive\\Pictures"; // Modify this path based on your OS and access rights
+        File rootDir = new File(rootPath);
+        printFiles(rootDir);
+    }
+
+    public static void printFiles(File directory) {
+        // Check if directory exists and is actually a directory
+        if (directory.exists() && directory.isDirectory()) {
+            // Get list of files and subdirectories
+            File[] files = directory.listFiles();
+
+            // Iterate through each file/subdirectory
+            for (File file : files) {
+                if (file.isFile()) {
+                    // Print filename if it's a file
+                    System.out.println(file.getAbsolutePath()); // Absolute path of the file
+                } else if (file.isDirectory()) {
+                    // Recursive call for subdirectories (explore deeper)
+                    printFiles(file);
+                }
+            }
+        }
     }
 }
